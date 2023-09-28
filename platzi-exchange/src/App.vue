1df10b945171a45ca0068d689fae7a71fc17451c
Usuario <usuario@tcb-ddelavega.tecbaco.com>
2023-09-28 16:42:41 -0500

    Starting platzi-exchange project

---
+++ platzi-exchange/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import PxHeader from "@/components/PxHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    PxHeader,
+  },
+};
+
+</script>
+
+
+
+<template>
+  <main>
+    <PxHeader></PxHeader>
+  </main>
+</template>
+
+
+
+<style scoped></style>
---
+++ platzi-exchange/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import "@/assets/css/tailwind.css";
+
+createApp(App).mount('#app')
